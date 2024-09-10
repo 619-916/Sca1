57e7a121e6f4559ad8f4c7e7f6bfb6066b79986a
619-916 <148589115+619-916@users.noreply.github.com>
2024-09-10 19:35:21 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,20 @@
-print("hello world")
-print("im sca1")
+print("""
+Scaigsin yarrak gibi toluna hos geldiniz!
+
+seçenekler :
+      -ddos
+      -sex
+      -31
+""")
+seçim = input("Seçim yapin>>>")
+if seçim == "ddos":
+    print("DDOS HARAM KARDEŞ")
+elif seçim == "sex":
+    print("KONDOM VARMİ KARDES")
+elif seçim == 31:
+    print("ÇEKMEYİN OĞLUM,ERKEKLİĞİNİZ BİTER LAN! sikiniz dont get up the tomorrow lan!")
+else:
+    print("dogru yazsana amk cocu")
 
 
 
